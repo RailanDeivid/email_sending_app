5cf6c1a02ef6a88431d25759aab81ea447858f0b
Railan Deivid <contato.railandeivid@gmail.com>
2024-08-20 09:51:33 -0300

    update

--- app/Envio_de_E-mail.py
+++ app/Envio_de_E-mail.py
@@ -178,7 +178,7 @@ with cols2:
     selected_page = option_menu(
         menu_title=None,
         options=["Envio de E-mail", "Cadastro de Remetente"],
-        icons=["email", "file-earmark-break"],
+        icons=["bi bi-envelope-at", "gear"],
         menu_icon="cast",
         default_index=0,
         orientation="horizontal"
