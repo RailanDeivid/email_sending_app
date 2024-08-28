07521fbc70f609736394b9ce6c6221736369b49f
Railan Deivid <contato.railandeivid@gmail.com>
2024-08-28 12:41:39 -0300

    ajustes

--- app/Envio_de_E-mail.py
+++ app/Envio_de_E-mail.py
@@ -150,7 +150,7 @@ def enviar_emails():
                             col1, col2, col3, col4 = st.columns(4)
                             incluir_saudacao = col1.checkbox("Deseja incluir uma saudação?", value=False)
                             
-                            if incluir_saudacao ==  True:
+                            if incluir_saudacao:
                                 col_nome = col1.selectbox("Selecione a coluna com os nomes", df.columns.tolist())
                             else:
                                 col_nome = None
@@ -159,8 +159,6 @@ def enviar_emails():
                             cc_emails_global = st.text_input("CC Global: Copiado em todos os e-mails (Separados por vírgula)", "").split(',')
 
                             if st.button("Enviar E-mails"):
-                                total_emails = len(df_selecionado)
-                                emails_enviados = 0
                                 for _, row in df_selecionado.iterrows():
                                     email = row[col_email]
                                     nome = row.get(col_nome, "") if col_nome else ""
@@ -176,10 +174,6 @@ def enviar_emails():
                                     elif not enviar_anexos:
                                         send_email(email, None, subject, corpo_email, cc_emails_global + cc_emails_spec)
                                         st.success(f"Email enviado para {email} sem anexo e em CC para {', '.join(cc_emails_global + cc_emails_spec)}.")
-                                        
-                                    emails_enviados += 1
-                                if emails_enviados == total_emails:
-                                    st.warning("Todos os e-mails foram enviados com sucesso!")
                         else:
                             st.warning("Alguns anexos não correspondem aos nomes escolhidos como (nomes dos arquivos). Verifique se os arquivos estão corretos.")
                 else:
@@ -188,8 +182,6 @@ def enviar_emails():
                     cc_emails_global = st.text_input("CC Global: Copiado em todos os e-mails (Separados por vírgula)", "").split(',')
 
                     if st.button("Enviar E-mails"):
-                        total_emails = len(df_selecionado)
-                        emails_enviados = 0
                         for _, row in df_selecionado.iterrows():
                             email = row[col_email]
                             nome = row.get(col_nome, "") if col_nome else ""
@@ -198,9 +190,6 @@ def enviar_emails():
                             cc_emails_spec = [cc.strip() for cc in row[col_cc].split(',')] if col_cc and pd.notna(row[col_cc]) else []
                             send_email(email, None, subject, corpo_email, cc_emails_global + cc_emails_spec)
                             st.success(f"Email enviado para {email} sem anexo e em CC para {', '.join(cc_emails_global + cc_emails_spec)}.")
-                            emails_enviados += 1
-                        if emails_enviados == total_emails:
-                            st.warning("Todos os e-mails foram enviados com sucesso!")
             else:
                 st.error("Por favor, selecione ao menos um e-mail para processar.")
         else:
