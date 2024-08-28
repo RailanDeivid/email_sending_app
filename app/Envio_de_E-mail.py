f08d3be976d87a0d472ae21c118d3130c7ece8ca
Railan Deivid <contato.railandeivid@gmail.com>
2024-08-28 12:07:18 -0300

    update

--- app/Envio_de_E-mail.py
+++ app/Envio_de_E-mail.py
@@ -61,6 +61,7 @@ def usar_credenciais():
         st.error("Nenhum email cadastrado. Por favor, cadastre o remetente na página de Cadastro de Remetente.")
         return False
 
+
 # Função de envio de emails
 def enviar_emails():
     st.title("Envio de Emails em Massa")
@@ -158,8 +159,6 @@ def enviar_emails():
                             cc_emails_global = st.text_input("CC Global: Copiado em todos os e-mails (Separados por vírgula)", "").split(',')
 
                             if st.button("Enviar E-mails"):
-                                total_emails = len(df_selecionado)
-                                emails_enviados = 0
                                 for _, row in df_selecionado.iterrows():
                                     email = row[col_email]
                                     nome = row.get(col_nome, "") if col_nome else ""
@@ -175,12 +174,6 @@ def enviar_emails():
                                     elif not enviar_anexos:
                                         send_email(email, None, subject, corpo_email, cc_emails_global + cc_emails_spec)
                                         st.success(f"Email enviado para {email} sem anexo e em CC para {', '.join(cc_emails_global + cc_emails_spec)}.")
-                                        
-                                    emails_enviados += 1
-                                    
-                                if emails_enviados == total_emails:
-                                    st.warning("Todos os e-mails foram enviados com sucesso!")
-                        
                         else:
                             st.warning("Alguns anexos não correspondem aos nomes escolhidos como (nomes dos arquivos). Verifique se os arquivos estão corretos.")
                 else:
@@ -197,8 +190,6 @@ def enviar_emails():
                             cc_emails_spec = [cc.strip() for cc in row[col_cc].split(',')] if col_cc and pd.notna(row[col_cc]) else []
                             send_email(email, None, subject, corpo_email, cc_emails_global + cc_emails_spec)
                             st.success(f"Email enviado para {email} sem anexo e em CC para {', '.join(cc_emails_global + cc_emails_spec)}.")
-                    
-                        
             else:
                 st.error("Por favor, selecione ao menos um e-mail para processar.")
         else:
