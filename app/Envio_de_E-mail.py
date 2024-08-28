be5da64a8568704e4ddfe5e76be5f0ee50b9e828
Railan Deivid <contato.railandeivid@gmail.com>
2024-08-28 11:58:18 -0300

    update

--- app/Envio_de_E-mail.py
+++ app/Envio_de_E-mail.py
@@ -189,7 +189,8 @@ def enviar_emails():
                             cc_emails_spec = [cc.strip() for cc in row[col_cc].split(',')] if col_cc and pd.notna(row[col_cc]) else []
                             send_email(email, None, subject, corpo_email, cc_emails_global + cc_emails_spec)
                             st.success(f"Email enviado para {email} sem anexo e em CC para {', '.join(cc_emails_global + cc_emails_spec)}.")
-                        st.warning(f"Todos os E-mails foram enviados com sucesso!!")
+                    st.warning(f"Todos os E-mails foram enviados com sucesso!!")
+                        
             else:
                 st.error("Por favor, selecione ao menos um e-mail para processar.")
         else:
